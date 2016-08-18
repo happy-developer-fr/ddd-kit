7efb2b0d9cd14169bd5e44328e46b13a06f9d98c
COTTET Julien <tetto@gmail.com>
2016-08-18 19:07:08 +0200

    first commit

---
+++ src/main/java/annotations/Aggregate.java
@@ -0,0 +1,14 @@
+package annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by tettoc on 18/08/2016.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Aggregate {
+}
